e6504211da25d0d03820a13578af4dded26acccd
Horacio Camacho <camacho19992012@gmail.com>
2022-03-08 10:48:27 -0500

    login form

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
+import { UsuarioService } from 'src/app/services/usuario.service';
+
+declare const gapi:any;
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ["./login.component.scss"],
+  styleUrls: [ './login.component.css' ]
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public formSubmitted = false;
+  public auth2: any;
+
+  public loginForm = this.fb.group({
+    Usuario: [ '' , [ Validators.required, Validators.email ] ],
+    password: ['', Validators.required ],
+    remember: [false]
+  });
+
+
+  constructor( private router: Router,
+               private fb: FormBuilder,
+               private usuarioService: UsuarioService,
+               private ngZone: NgZone ) { }
 
   ngOnInit(): void {
+    //this.renderButton();
   }
-
 }
+
+
